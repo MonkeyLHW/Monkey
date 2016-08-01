390b37875e3ce8951aaac33da90b915cbc281e73
dlt-lihongwei <915576784@qq.com>
2016-08-01 09:22:25 +0800

    2233

--- Monkey/Form1.Designer.cs
+++ Monkey/Form1.Designer.cs
@@ -36,7 +36,7 @@ namespace Monkey
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(251, 221);
+            this.button1.Location = new System.Drawing.Point(194, 216);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
