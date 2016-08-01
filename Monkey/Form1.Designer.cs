c077fbef12d7d18487c0e0914999260eda1719a8
ShenBing <535729802@qq.com>
2016-08-01 09:57:14 +0800

    66666

--- Monkey/Form1.Designer.cs
+++ Monkey/Form1.Designer.cs
@@ -29,49 +29,54 @@ namespace Monkey
         private void InitializeComponent()
         {
             this.button1 = new Gizmox.WebGUI.Forms.Button();
-            this.comboBox1 = new Gizmox.WebGUI.Forms.ComboBox();
             this.label1 = new Gizmox.WebGUI.Forms.Label();
-            this.listView1 = new Gizmox.WebGUI.Forms.ListView();
+            this.directorySearcher1 = new System.DirectoryServices.DirectorySearcher();
+            this.listBox1 = new Gizmox.WebGUI.Forms.ListBox();
+            this.listBox2 = new Gizmox.WebGUI.Forms.ListBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(208, 255);
+            this.button1.Location = new System.Drawing.Point(161, 298);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(129, 141);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 1;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(170, 304);
+            this.label1.Location = new System.Drawing.Point(182, 240);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 2;
             this.label1.Text = "label1";
             // 
-            // listView1
+            // directorySearcher1
             // 
-            this.listView1.DataMember = null;
-            this.listView1.Location = new System.Drawing.Point(46, 265);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(100, 100);
-            this.listView1.TabIndex = 3;
+            this.directorySearcher1.ClientTimeout = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerPageTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            this.directorySearcher1.ServerTimeLimit = System.TimeSpan.Parse("-00:00:01");
+            // 
+            // listBox1
+            // 
+            this.listBox1.Location = new System.Drawing.Point(67, 92);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(100, 95);
+            this.listBox1.TabIndex = 3;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Location = new System.Drawing.Point(232, 92);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(100, 95);
+            this.listBox2.TabIndex = 4;
             // 
             // Form1
             // 
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
             this.Size = new System.Drawing.Size(392, 410);
             this.Text = "Form1";
@@ -82,8 +87,9 @@ namespace Monkey
         #endregion
 
         private Gizmox.WebGUI.Forms.Button button1;
-        private Gizmox.WebGUI.Forms.ComboBox comboBox1;
         private Gizmox.WebGUI.Forms.Label label1;
-        private Gizmox.WebGUI.Forms.ListView listView1;
+        private System.DirectoryServices.DirectorySearcher directorySearcher1;
+        private Gizmox.WebGUI.Forms.ListBox listBox1;
+        private Gizmox.WebGUI.Forms.ListBox listBox2;
     }
 }
